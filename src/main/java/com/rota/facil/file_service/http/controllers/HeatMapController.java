d77ae295afa549b98a2ef9db62f0bb3bdeafd9bc
allana-clarice <antoniaallana536@gmail.com>
2026-05-18 04:42:15 -0300

    feat: add more endpoint's to heatMap

--- src/main/java/com/rota/facil/file_service/http/controllers/HeatMapController.java
+++ src/main/java/com/rota/facil/file_service/http/controllers/HeatMapController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,5 +26,35 @@ public class HeatMapController {
             @AuthenticationPrincipal CurrentUser currentUser
             ) {
         return ResponseEntity.ok(fileService.upload(currentUser, routeId, file, FileCategory.ROUTE_BOARD_POINT_HEAT_MAP));
+    }
+
+    @GetMapping("/{routeId}/all")
+    public ResponseEntity<List<FileResponseDTO>> listAllRouteHeatMaps(
+            @PathVariable UUID routeId
+    ) {
+        return ResponseEntity.ok(fileService.listByOwnerIdAndCategory(routeId, FileCategory.ROUTE_BOARD_POINT_HEAT_MAP));
+    }
+
+    @GetMapping("/{fileId}")
+    public ResponseEntity<FileResponseDTO> fetchHeatPhoto(@PathVariable UUID fileId) {
+        return ResponseEntity.ok(fileService.fetchByIdAndCategory(fileId, FileCategory.BOARD_POINT_PIC));
+    }
+
+    @PutMapping("/{fileId}")
+    public ResponseEntity<FileResponseDTO> updateHeatMap(
+            @PathVariable UUID fileId,
+            @AuthenticationPrincipal CurrentUser currentUser,
+            @RequestPart(name = "file") MultipartFile multipartFile
+    ) {
+        return ResponseEntity.ok(fileService.update(currentUser, fileId, multipartFile));
+    }
+
+    @DeleteMapping("/{fileId}")
+    public ResponseEntity<Void> deleteHeatMap(
+            @PathVariable UUID fileId,
+            @AuthenticationPrincipal CurrentUser currentUser
+    ) {
+        fileService.delete(currentUser, fileId);
+        return ResponseEntity.ok().build();
     }
 }
