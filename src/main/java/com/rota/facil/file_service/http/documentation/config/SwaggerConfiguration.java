e0fc57ce856d8fd17d3be281e5e2cf30bd0cfb26
Thauã Gabriel <tagashi.dev@gmail.com>
2026-07-02 17:33:32 -0300

    refactor: adjusting open api definition

--- src/main/java/com/rota/facil/file_service/http/documentation/config/SwaggerConfiguration.java
+++ src/main/java/com/rota/facil/file_service/http/documentation/config/SwaggerConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Audit Service",
+                title = "File Service",
                 version = "1.0"
         )
 )
